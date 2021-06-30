54111ce81212ece4c06cccc4a7892d9a6fdebb89
Daniel07816 <danielbehar@ufm.edu>
2021-06-29 18:02:23 -0600

    investigue sobre graficas en python y este archivo contiene lo necesario y comentado

--- Pruebas/Graf.py
+++ Pruebas/Graf.py
@@ -1,7 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-y=[2,4,6]
-plt.plot(y)
+#datos de prueba (numpy solo es para probar, no es necesario al graficar de verdad en el proyecto)
+x=np.linspace(-3,3,51)
+y=x**2
+
+#graficador + etiquetas
+plt.plot(x,y,'b.',label=r'$y_1$')
+plt.xlabel('x')
+plt.ylabel(r'$y=x^2$') #se pone r para formato "raw" y los $ encierran el texto para armarlo mas legible
+
+plt.title('f(x)')
+plt.legend(loc=1)
+plt.grid(True)
+
+plt.xticks(np.linspace(-3,3,14)) #(punto inicial, punto final, cantidad de separaciones entre a y b)
+#si se quiere separar pero en y se escribe: plt.yticks
+
+#linea horizontal o vertical
+plt.axhline(4,color='r',lw=2)
+#si se quiere la linea recta vertical es: plt.axvline. Horizontal: plt.axhline
 
 plt.show()
