40589ecd04e3de2550f789bf3412a90f640cf3e0
CruzdelCid <cldelcid@ufm.edu>
2021-07-06 11:17:30 -0600

    MOdificación de bisección

--- Pruebas/math/Biseccion.py
+++ Pruebas/math/Biseccion.py
@@ -1,3 +1,4 @@
+from numpy import arange 
 import numpy as np
 import sympy as sym
 
@@ -6,8 +7,6 @@ x=sym.Symbol('x')
 def Bis(f,a,b):
     fa=f.subs(x,a)
     fb=f.subs(x,b)
-    print(fa)
-    print(fb)
 
     if fa*fb==0:
         return None
@@ -23,6 +22,26 @@ def Bis(f,a,b):
             fa=fp
 
 #prueba
+vector = [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+rango = arange(min(vector)-1, max(vector)+1, 0.3)
 fx=x*x-1
-res=Bis(fx,-2,0)
+
+anterior = 0.0 
+actual = 0.0 
+rangos = []
+for i in range(len(rango)):
+    anterior = actual 
+    actual = fx.subs(x,rango[i])
+    print([anterior,actual])
+    if anterior*actual < 0: 
+        rangos.append([rango[i-1],rango[i]])
+
+print(rangos)
+# print(Bis(fx,2.84217094304040e-14,-0.189999999999974))
+# print(Bis(fx,-4.26325641456060e-14,0.209999999999952))
+
+res = []
+for i in rangos: 
+    res.append(Bis(fx,i[0],i[1]))
+
 print(res)
